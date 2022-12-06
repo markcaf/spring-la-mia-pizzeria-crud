d73497185aeab6ec876632f8150fc8c390f525aa
Marco Cafasso <marcocafassog@gmail.com>
2022-12-06 15:15:14 +0100

    feat: add autowired in SpringBootApplication to invoke drinkService

--- Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
+++ Pizzeria/src/main/java/org/generation/italy/demo/PizzeriaApplication.java
@@ -2,7 +2,9 @@ package org.generation.italy.demo;
 
 import java.util.List;
 
+import org.generation.italy.demo.pojo.Drink;
 import org.generation.italy.demo.pojo.Pizza;
+import org.generation.italy.demo.serv.DrinkService;
 import org.generation.italy.demo.serv.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -14,6 +16,7 @@ public class PizzeriaApplication implements CommandLineRunner {
 
 	@Autowired
 	private PizzaService pizzaService;
+	private DrinkService drinkService;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(PizzeriaApplication.class, args);
@@ -22,7 +25,7 @@ public class PizzeriaApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		// INSERIMENTO
+		// INSERIMENTO PIZZE
 
 		Pizza p1 = new Pizza("Margherita", "Mozzarella, olio e pomodoro", 4);
 		Pizza p2 = new Pizza("Marinara", "Pomodoro, aglio, origano", 3);
@@ -33,10 +36,26 @@ public class PizzeriaApplication implements CommandLineRunner {
 		pizzaService.save(p2);
 		pizzaService.save(p3);
 		pizzaService.save(p4);
+		
+		// INSERIMENTO DRINK
+
+		Drink d1 = new Drink("Coca Cola", "Bevanda gassata analcolica", 3);
+		Drink d2 = new Drink("Vino", "Rosso o bianco della casa", 6);
+		Drink d3 = new Drink("Sprite", "Bevanda gassata analcolica", 3);
+		Drink d4 = new Drink("Birra", "Birra chiara artigianale", 7);
+
+		drinkService.save(d1);
+		drinkService.save(d2);
+		drinkService.save(d3);
+		drinkService.save(d4);
+
 
 		// LETTURA
 
 		List<Pizza> pizzas = pizzaService.findAll();
 		System.out.println(pizzas);
+		
+		List<Drink> drinks = drinkService.findAll();
+		System.out.println(drinks);
 	}
 }
